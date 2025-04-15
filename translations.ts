92cc6947951dc150275cc187a5adaf7c469ceda0
Felix <felix@dieblasers.de>
2025-04-15 09:42:02 +0200

    hero translated

--- translations.ts
+++ translations.ts
@@ -1,15 +1,20 @@
 export const translations = {
     en: {
-      hero: {
-        name: 'Felix Blaser',
-        roles: ['Project Manager', 'Frontend Developer', 'Digital Strategist'],
-        cta: "Let’s Talk!",
-      },
+        hero: {
+            iam: {
+                text: 'I am',
+                class: 'heroIam-en',
+            },
+            name: 'Felix Blaser',
+            roles: ['Project Manager', 'Frontend Developer', 'Digital Strategist'],
+            cta: 'Let’s Talk!',
+            scroller: 'scroll down',
+          },
       about: {
         title: 'About me',
         intro: "Hi, my name is Felix and I'm a project manager, front-end web developer and digital strategist based in the most beautiful city in the world, Munich, Germany. <br> I started my career as an Event Manager, handling projects for the automotive and pharmaceutical industry.",
-        experience: 'I have gained experience in...',
-        change: "After many years of...",
+        experience: 'I have gained experience in event and tradeshows for automotive, pharma and various other branches. I have worked with clients like BMW, BMS, Gilead and many more. <br> I have a strong background in project management and digital strategy, which allows me to understand the needs of my clients and deliver results that exceed their expectations.',
+        change: "After many years of successful projects, I've decided to make a change and broaden my portfolio. That's why you see this website.<br> I'm combining project management with technical and web development skills to meet today's employer needs.",
       },
       skills: {
         title: 'Skills',
@@ -36,16 +41,21 @@ export const translations = {
     },
   
     de: {
-      hero: {
-        name: 'Felix Blaser',
-        roles: ['Projektmanager', 'Frontend-Entwickler', 'Digitaler Stratege'],
-        cta: "Lass uns sprechen!",
-      },
+        hero: {
+            iam: {
+                text: 'Ich bin',
+                class: 'heroIam-de',
+              },
+            name: 'Felix Blaser',
+            roles: ['Projektmanager', 'Frontend-Entwickler', 'Digitalstratege'],
+            cta: 'Lass uns sprechen!',
+            scroller: 'hier lang',
+          },
       about: {
         title: 'Über mich',
-        intro: 'Hi, ich bin Felix und arbeite als...',
-        experience: 'Ich habe Erfahrung in...',
-        change: 'Nach vielen erfolgreichen Jahren...'
+        intro: 'Hi, ich bin Felix und arbeite als Projektmanager, Frontend-Entwickler und digitaler Stratege in der schönsten Stadt der Welt, München. <br> Ich habe meine Karriere als Event-Manager begonnen und Projekte für die Automobil- und Pharmaindustrie geleitet.',
+        experience: 'Ich habe Erfahrung in Events und Messen für die Automobil-, Pharma- und viele andere Branchen. Ich habe mit Kunden wie BMW, BMS, Gilead und vielen anderen zusammengearbeitet. <br> Ich habe einen starken Hintergrund im Projektmanagement und in der digitalen Strategie, was es mir ermöglicht, die Bedürfnisse meiner Kunden zu verstehen und Ergebnisse zu liefern, die ihre Erwartungen übertreffen.',
+        change: 'Nach vielen erfolgreichen Jahren in der Projektarbeit habe ich beschlossen, eine Veränderung vorzunehmen und mein Portfolio zu erweitern. Deshalb sehen Sie diese Website.<br> Ich kombiniere Projektmanagement mit technischen und Webentwicklungsfähigkeiten, um den heutigen Anforderungen der Arbeitgeber gerecht zu werden.'
       },
       skills: {
         title: 'Skills',
