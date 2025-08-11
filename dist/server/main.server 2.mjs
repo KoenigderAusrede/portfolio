988f7f6b58b6d4ab59ccca18084216e35b35302f
Felix <felix@dieblasers.de>
2025-08-11 18:20:29 +0200

    responsive changes

---
+++ dist/server/main.server 2.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-WFMWFXTI.mjs";import"./chunk-ANBRCQ2U.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- dist/server/main.server.mjs
+++ dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WFMWFXTI.mjs";import"./chunk-ANBRCQ2U.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-FRGR2KVG.mjs";import"./chunk-QBURS2IX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
