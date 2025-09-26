4e0cab7800503b5469fa4824f43e311088226b8a
Felix <felix@dieblasers.de>
2025-09-26 17:06:31 +0200

    responsive fix prod ready

--- dist/server/main.server 3.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-FRGR2KVG.mjs";import"./chunk-QBURS2IX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- dist/server/main.server.mjs
+++ dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-EI2AHOSR.mjs";import"./chunk-QBURS2IX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-BKUHOMO7.mjs";import"./chunk-TTPRL7HA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
